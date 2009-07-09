7f583cd83b199a9f971bf4bebb2d22e3ce116c15
Malte S. Stretz <mss@apache.org>
2009-07-09 18:38:48 +0200

    Added package for global RS232 stuff.

---
+++ fhw_rs232/rs232_globals_p.vhd
@@ -0,0 +1,28 @@
+-----------------------------------------------------------------------
+-- Copyright (c) 2009 Malte S. Stretz <http://msquadrat.de> 
+--
+-- TODO
+-- 
+-----------------------------------------------------------------------
+-- This entity is part of the following library:
+-- pragma library fhw_rs232
+library fhw_rs232;
+
+library fhw_tools;
+use fhw_tools.types.all;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package rs232_globals_p is
+  
+  
+  
+end rs232_globals_p;
+
+package body rs232_globals_p is
+
+  
+  
+end rs232_globals_p;
