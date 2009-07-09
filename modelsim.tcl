e5812d2143451c0f888c069064a2f6bce45c8bab
Malte S. Stretz <mss@apache.org>
2009-07-09 14:02:25 +0200

    Add the ModelSim project.
    Copy mgc_location_map.template to mgc_location_map and add the path to
    this directory.  Oh, and the code in modelsim.tcl should work but
    doesn't. Yes, ModelSim stuill sucks, just not as much anymore.

---
+++ modelsim.tcl
@@ -0,0 +1,8 @@
+# This should work but doesn't. See
+# ModelSim User's Manual 6.4a Appendix B
+#  * http://www.model.com/resources/default.asp?sendto=/support/documentation/se/pdf_6.4a/modelsim_se_user.pdf
+# And
+#  * http://web.archive.org/web/20030419034330/http://www.model.com/resources/pref_variables/get_records_test.asp?Category=FileOperations
+
+#set SourceMap(SRC) "$env(MODELSIM)/.."
+#set SourceDir $SourceMap(SRC)
